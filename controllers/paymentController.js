27a56aa031dd0cb50d4c0c41dbaefb6420be773c
Atrio Technologies <161876931+atrio3@users.noreply.github.com>
2024-04-12 03:36:39 +0530

    Add files via upload

---
+++ controllers/paymentController.js
@@ -0,0 +1,43 @@
+import { instance } from "../server.js";
+import crypto from "crypto";
+
+export const checkout = async (req, res) => {
+  const options = {
+    amount: Number(req.body.amount * 100),
+    currency: "INR",
+  };
+
+  const order = await instance.orders.create(options);
+
+  res.status(200).json({
+    sucess: "true",
+    order,
+  });
+};
+
+export const paymentVerification = async (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+
+  const url =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5173"
+      : "https://www.nidirentals.com";
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(body.toString())
+    .digest("hex");
+
+  const isAuthentic = expectedSignature === razorpay_signature;
+
+  if (isAuthentic) {
+    res.redirect(`${url}/paymentsuccess?reference=${razorpay_payment_id}`);
+  } else {
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
---
+++ routes/paymentRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {
+  checkout,
+  paymentVerification,
+} from "../controllers/paymentController.js";
+
+const router = express.Router();
+
+router.route("/checkout").post(checkout);
+
+router.route("/paymentVerification").post(paymentVerification);
+
+export default router;
